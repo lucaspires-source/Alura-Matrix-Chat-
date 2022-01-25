f22aa98d924e1d954dc29a5a7bda9e05203c7c6d
lucaspires-source <pires.lucas94@gmail.com>
2022-01-25 14:50:37 -0300

    comecando

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function HomePage() {
+    return(
+        <div>
+            <h1>Boas Vindas de Volta!</h1>
+            <h2>Discord - Alura Matrix</h2>
+        </div>
+    )
+}
+
+export default HomePage
